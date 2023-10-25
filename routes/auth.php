40c25548fe1b932c3726c1caedf77e2e791e2aba
Teguera <aboubacarteguera21@example.com>
2023-10-25 10:11:45 +0000

    conflit auth.php resolu

--- routes/auth.php
+++ routes/auth.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['admin','auth'])->group(function () {
+Route::middleware(['auth','admin'])->group(function () {
 
     Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
     Route::post('register', [RegisteredUserController::class, 'store']);
